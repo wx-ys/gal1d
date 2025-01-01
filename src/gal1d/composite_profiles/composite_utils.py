dec3a15f593f15ba0f478ed41a1d05d94c8fdc2e
wx-ys <2035180080@qq,com>
2025-01-01 18:54:01 +0800

    parameters num

--- src/gal1d/composite_profiles/composite_utils.py
+++ src/gal1d/composite_profiles/composite_utils.py
@@ -7,7 +7,8 @@ import inspect
 def get_required_params_num(func):
     require_num = 0
     for name,param in inspect.signature(func).parameters.items():
-        if param.default == inspect.Parameter.empty:
+        if ((param.kind != param.VAR_POSITIONAL and param.kind != param.VAR_KEYWORD) 
+            and param.default == inspect.Parameter.empty):
             require_num = require_num +1
     return require_num
     
